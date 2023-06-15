33e52d8580807afce605d6217ddd35673e54f3ea
philehteo <m.theophile.arnould@gmail.com>
2023-06-15 11:07:51 +0200

    shell pipes

--- addOne.c
+++ addOne.c
@@ -1,2 +1,14 @@
-a = input(int(),"enter a number")
-print(a+1)
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+
+int main(){
+        int number;
+        printf("entrez un nombre : ");
+        scanf("%d", &number);
+        printf("ton nombre est : %i\n",number+1);
+
+    return 0;
+}
--- shellPipes.c
+++ shellPipes.c
@@ -13,26 +13,66 @@ void printArguments(char* args[]) {
     }
 }
 
-int execute(char* args[MAX_COMMAND_LENGTH / 2 + 1]){
-    // Fork a child process
-    pid_t pid = fork();
 
-    if (pid < 0) {
-        fprintf(stderr, "Failed to fork a child process.\n");
-        return 1;
-    } else if (pid == 0) {
-        // Child process
-        
-        execvp(args[0], args);
-        
-        fprintf(stderr, "Failed to execute the command.\n");
-        return 1;
-    } else {
-        // Parent process
-        wait(NULL);
+void executePipe(char* args[MAX_PIPE][MAX_COMMAND_LENGTH], int num_commands){
+    num_commands += 1;
+    int pipes[num_commands][2];//array of all file director that will be used
+
+    for (size_t i = 0; i < num_commands; i++)
+    {
+        if(i < num_commands - 1){
+                pipe(pipes[i]);
+            }
+        pid_t pid = fork();
+
+        if (pid < 0) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            // Child process
+
+            if (i > 0) {//if not the first command read the output of previous process
+                if( (dup2(pipes[i-1][0], STDIN_FILENO)) <0){
+                    perror("dup2");
+                    exit(EXIT_FAILURE);
+                }
+                close(pipes[i-1][0]);
+                close(pipes[i-1][1]);
+            }
+
+            if (i < num_commands - 1) {//if not the last cmd writ output in your pipe array for next process
+                close(pipes[i][0]);
+                if(dup2(pipes[i][1], STDOUT_FILENO)<0){
+                    perror("dup2");
+                    exit(EXIT_FAILURE);
+                }
+                close(pipes[i][1]);
+            }
+
+            execvp(args[i][0],args[i]);//exectute cmd with adequat IO
+            perror("execvp");
+            exit(EXIT_FAILURE);
+
+        }
+        else{
+
+            if (i > 0) {//close the actual fd before going to next
+                close(pipes[i-1][0]);
+                close(pipes[i-1][1]);
+            }
+
+            wait(NULL);//wait for the children to be exectued before starting the next children
+        }
     }
-    return 0;
+
+    if(num_commands > 1){
+        close(pipes[num_commands-1][0]);
+        close(pipes[num_commands-1][1]);
+    } //close the last IO fd
+
+
 }
+
 
 void changeDirectory(char* path) {
     if (chdir(path) == 0) {
@@ -58,11 +98,13 @@ void ChangeInput(char* filename){
 
 int main() {
     char command[MAX_COMMAND_LENGTH];
-    char commands[MAX_PIPE][MAX_COMMAND_LENGTH];
-    char* args[MAX_COMMAND_LENGTH / 2 + 1];  // +1 for NULL terminator
+    char * args[MAX_PIPE][MAX_COMMAND_LENGTH];  // +1 for NULL terminator
+
     int originalStdout = dup(STDOUT_FILENO);
 
     while (1) {
+            int CommandNumber = 0;
+
         printf("Shell> ");
         fgets(command, MAX_COMMAND_LENGTH, stdin);
         // Remove newline character from the command
@@ -91,31 +133,38 @@ int main() {
             }
             else if (strcmp(token, "|") == 0){
                 //redirect the IO for pipes 
+                args[CommandNumber][i] = NULL;
+                i=0;
+
+                CommandNumber += 1;
+                token = strtok(NULL, " ");
             }
             else{
-                args[i] = token;
+                args[CommandNumber][i] = token;
                 token = strtok(NULL, " ");
                 i++;
             }
             
-
         }
-        args[i] = NULL;
-        if(strcmp(args[0], "cd") == 0){
-            if (args[1] != NULL) {
-                changeDirectory(args[1]);
+        args[CommandNumber][i] = NULL; 
+        
+        //change working direcotry of the parent processus so it transmit to all child after
+        if(strcmp(args[0][0], "cd") == 0){
+            if (args[0][1] != NULL) {
+                changeDirectory(args[0][1]);
             } else {
                 fprintf(stderr, "cd: Missing argument.\n");
             }
         }
+        //
         else{
-            if(execute(args) == 1) {
-                return 1;
-            }
+            executePipe(args,CommandNumber);
         }
+
         // redirection des sorties standar vers le shell si elles ont étés changés
         dup2(originalStdout,STDOUT_FILENO);
         dup2(originalStdout, STDIN_FILENO);
+        
     }
 
     return 0;
